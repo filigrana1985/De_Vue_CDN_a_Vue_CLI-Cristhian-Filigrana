61a8bf831ce5d15fbe128cd3dff2d2d464983692
Cristhian Filigrana <filigrana1985@gmail.com>
2023-06-13 21:36:11 -0500

    vue de CDN a CLI

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <table>
+      <tr>
+        <th>Nombre</th>
+        <th>Edad</th>
+        <th>Correo electrónico</th>
+      </tr>
+      <row-component v-for="item in data" :key="item.id" :row-data="item"></row-component>
+    </table>
+  </div>
+</template>
+
+<script>
+  import RowComponent from './components/RowComponent.vue';
+
+  export default {
+    name:'app',
+    components: {
+      RowComponent,
+    },
+    data() {
+      return {
+        data: [
+          { id: 1, nombre: 'Juan', edad: 25, correo: 'juan@example.com' },
+          { id: 2, nombre: 'María', edad: 30, correo: 'maria@example.com' },
+          { id: 3, nombre: 'Pedro', edad: 28, correo: 'pedro@example.com' }
+        ]
+      };
+    }
+  };
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  
+}
+#table {
+    border-collapse: collapse;
+    width: 100%;
+  }
+
+  th, td {
+    border: 1px solid black;
+    padding: 8px;
+    text-align: left;
+  }
+</style>
---
+++ src/components/RowComponent.vue
@@ -0,0 +1,21 @@
+<template>
+   
+    <tr>
+      <td>{{ rowData.nombre }}</td>
+      <td>{{ rowData.edad }}</td>
+      <td>{{ rowData.correo }}</td>
+    </tr>
+  </template>
+  
+  <script>
+    export default {
+      props: {
+        rowData: {
+          type: Object,
+          required: true
+        }
+      }
+    };
+  </script>
+  
+  
